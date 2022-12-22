f756d73f65a74cc44d5dcfc7cc23bb7ea180ad05
varshajeevan <varsha.jeevan@citrusinformatics.com>
2022-12-22 22:33:41 +0530

    Activity added for dev

--- app/src/main/java/com/app/airbook/AirBookMainPage.java
+++ app/src/main/java/com/app/airbook/AirBookMainPage.java
@@ -2,13 +2,25 @@ package com.app.airbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.app.airbookfeatures.PackageBridge;
 
 public class AirBookMainPage extends AppCompatActivity {
-
+    private Button redirectBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_air_book_main_page);
+        setContentView(R.layout.activity_dev_page);
+        redirectBtn = findViewById(R.id.redirectBtn);
+        redirectBtn.setOnClickListener(v -> {
+            PackageBridge packageBridge = new PackageBridge(this);
+            packageBridge.renderMainPage();
+           /* Intent myIntent = new Intent(AirBookMainPage.this, LibraryMain.class);
+            //myIntent.putExtra("key", value); //Optional parameters
+            AirBookMainPage.this.startActivity(myIntent);*/
+        });
     }
 }
